91bf159e3294f3025b99c7a23afcf529177f5aaf
zyr <zyr@nj.com>
2025-09-22 21:25:14 +0800

    配置文件修改

---
+++ src/gameconfig.go
@@ -0,0 +1,57 @@
+package gameconfig
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+)
+
+type gameConfig struct {
+	*Tables
+}
+
+var gameCfg *gameConfig
+
+func init() {
+	gameCfg = NewGameConfig()
+	if err := gameCfg.OnInit(context.Background()); err != nil {
+		panic(err)
+	}
+}
+
+func GameConfig() *gameConfig {
+	return gameCfg
+}
+
+func NewGameConfig() *gameConfig {
+	gameCfg = &gameConfig{}
+	return gameCfg
+}
+
+func (c *gameConfig) OnInit(ctx context.Context) error {
+	if err := c.initTables(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (gc *gameConfig) initTables() error {
+	tables, err := NewTables(loader)
+	if err != nil {
+		return err
+	}
+	gc.Tables = tables
+	return nil
+}
+
+func loader(file string) ([]map[string]interface{}, error) {
+	if bytes, err := os.ReadFile("gameconfig/data/" + file + ".json"); err != nil {
+		return nil, err
+	} else {
+		jsonData := make([]map[string]interface{}, 0)
+		if err = json.Unmarshal(bytes, &jsonData); err != nil {
+			return nil, err
+		}
+		return jsonData, nil
+	}
+}
