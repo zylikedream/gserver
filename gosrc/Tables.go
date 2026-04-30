4624c5550208b52ee816019c0a13cbafa9271816
zhangyi <zhangyi-home-pc@home.com>
2026-04-30 15:53:52 +0800

    sync: 配置表更新 2026-04-30 15:53

--- gosrc/Tables.go
+++ gosrc/Tables.go
@@ -14,6 +14,7 @@ type JsonLoader func(string) ([]map[string]interface{}, error)
 type Tables struct {
     TbItem *GardenTbItem
     TbFlower *GardenTbFlower
+    TbGardenPlot *GardenTbGardenPlot
     TbItemTag *GardenTbItemTag
     TbGlobalConfig *GardenTbGlobalConfig
 }
@@ -33,6 +34,12 @@ func NewTables(loader JsonLoader) (*Tables, error) {
         return nil, err
     }
     if tables.TbFlower, err = NewGardenTbFlower(buf) ; err != nil {
+        return nil, err
+    }
+    if buf, err = loader("garden_tbgardenplot") ; err != nil {
+        return nil, err
+    }
+    if tables.TbGardenPlot, err = NewGardenTbGardenPlot(buf) ; err != nil {
         return nil, err
     }
     if buf, err = loader("garden_tbitemtag") ; err != nil {
